c98cc832d0885a738a762d7baba877df7737ce1c
meruem243 <mughees.ahmed243@gmail.com>
2023-07-25 21:12:41 +0500

    Play Sound Across Platforms

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # xylophone_a_simple_musical_instrument_app
 
 - Xylophone - A Simple Musical Instrument App
-  Practice App that allows to play sound
+  Practice App that allows to play sounds
 
 ## Things that will be covered after completing this app
 
 - What are Flutter & Dart Packages?
+    open source libraries of code /functions which can be implemented with minimum effort
+    to add libraries go to yaml file : using audio player package as eg :
+  dependencies:
+    flutter:
+      sdk: flutter
+    audioplayers: ^4.1.0
+    then in main.dart -> import 'package:audioplayers/audioplayers.dart';
+
 - How to Play Sound Across Platforms 
+    using flutter package audioplayers: ^4.1.0 to play sound
+    final player = AudioPlayer(); //
+    player.play(AssetSource('note1.wav'));
+    enableFeedback: false, // to disable the sound effect of button itself
+
 - How to Play Multiple Sounds 
 - Dart Functions - Part 2
 - Updating the UI of Our App
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+// import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(const XylophoneApp());
 
@@ -12,9 +13,36 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text(
-              nouns.first,
-              style: const TextStyle(fontSize: 50.0),
+            // child: TextButton(
+            //   onPressed: () {
+            //     final player = AudioPlayer();
+            //     // player.setSourceAsset('note4.wav');
+            //     player.play(AssetSource('note7.wav'));
+            //     // print('button got pressed');
+            //
+            //     // player.loadAsset(AssetSource('note1.wav'));
+            //     // Null;
+            //     // await player.play('note1.wav');
+            //     // player.play(Source : );
+            //     // await player.setSource(
+            //     //   AssetSource('note1.wav'),
+            //   },
+            //   style: const ButtonStyle(
+            //     enableFeedback: false, // Disable the sound effect
+            //   ),
+            //   child: const Text('click me'),
+            // ),
+            child: MaterialButton(
+              onPressed: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note1.wav'));
+              },
+              enableFeedback: false, // Disable the sound effect
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Text('Press me'),
             ),
           ),
         ),
